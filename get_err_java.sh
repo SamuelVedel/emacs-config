192d2638d744b41602cb849efbd751af8da661cb
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-02-24 15:37:04 +0100

    fix: l'emplacement du zut.txt

--- get_err_java.sh
+++ get_err_java.sh
@@ -10,15 +10,16 @@
 
 file=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 2)
 dir=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 1)
+zut_file=~/emacs-config/zut.txt
 
 function exec_make() {
 	if [ -f $1/Makefile ]
 	then
 		#echo $dir
 		#echo $file
-		#make -C $1 $(echo $file | sed 's/.java/.class/') 2> zut.txt > /dev/null
-		javac -encoding iso-8859-1 -d $dir/bin/ -cp $dir/src/ $dir/src/$file 2> zut.txt > /dev/null
-		cat zut.txt | grep /$file | cut -d ':' -f 2 | uniq
+		#make -C $1 $(echo $file | sed 's/.java/.class/') 2> $zut_file > /dev/null
+		javac -encoding iso-8859-1 -d $dir/bin/ -cp $dir/src/ $dir/src/$file 2> $zut_file > /dev/null
+		cat $zut_file | grep /$file | cut -d ':' -f 2 | uniq
 		exit
 	fi
 }
