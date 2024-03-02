c5e17a817701766b5c5d590704668ba8bcc18334
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-03-02 20:22:47 +0100

    more syntaxic coloration for java-mode

--- syntax-color.el
+++ syntax-color.el
@@ -2,12 +2,33 @@
 ;;                                      "\\( \\|^\\|(\\|\\.\\)[a-zA-Z1-9]+ *("
 ;;                                      . font-lock-function-name-face)))
 
+;;(defconst char-befor-words "\\( \\|(\\|)\\|\\.\\|^\\)")
+
 (font-lock-add-keywords
  'java-mode
- '(("\\([a-zA-Z1-9_]+\\) *("
+ ;;'(("\\([a-zA-Z1-9_]+\\) *("
+ '(("\\([a-z][a-zA-Z1-9]*\\) *("
     1
     'font-lock-function-name-face
     nil
+    )
+   ("[^a-zA-Z1-9_]\\([a-z][a-zA-Z1-9]*\\)"
+    1
+    'font-lock-variable-name-face
+    nil
+    )
+   ("[^a-zA-Z1-9_]\\([A-Z][A-Z1-9_]*\\)"
+    1
+    'font-lock-constant-face
+    nil
+    )
+   ("[^a-zA-Z1-9_]\\([A-Z][a-zA-Z1-9]*\\)\\."
+    1
+    'font-lock-type-face
+    t
     ))
  t
  )
+
+;;(highlight-regexp "[a-zA-Z1-9]")
+;;(unhighlight-regexp "[a-zA-Z1-9]")
