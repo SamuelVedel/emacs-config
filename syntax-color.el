faf9fc599cd154b4446d5706d856033defe42f79
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-03-05 00:09:50 +0100

    feat: un peu de coloration syntaxique pour c

--- syntax-color.el
+++ syntax-color.el
@@ -36,5 +36,25 @@
  t
  )
 
+(font-lock-add-keywords
+ 'c-mode
+ '(("\\([a-z][a-zA-Z0-9_]*\\) *("
+    1
+    'font-lock-function-name-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([a-z][a-zA-Z0-9_]*\\)"
+    1
+    'font-lock-variable-name-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([A-Z][A-Z0-9_]*\\)"
+    1
+    'font-lock-constant-face
+    nil
+    ))
+ t
+ )
+
 ;;(highlight-regexp "[a-zA-Z1-9]")
 ;;(unhighlight-regexp "[a-zA-Z1-9]")
