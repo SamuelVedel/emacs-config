432508bd85ecdb35f0f5c3f210c329f535be9b10
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-03-07 10:16:56 +0100

    syntaxic coloration for python and js

--- syntax-color.el
+++ syntax-color.el
@@ -56,5 +56,48 @@
  t
  )
 
+(font-lock-add-keywords
+ 'python-mode
+ '(("\\([a-z][a-zA-Z0-9_]*\\) *("
+    1
+    'font-lock-function-name-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
+    1
+    'font-lock-variable-name-face
+    nil
+    )
+   ;; ("[^a-zA-Z0-9_]\\([A-Z][A-Z0-9_]*\\)"
+   ;;  1
+   ;;  'font-lock-constant-face
+   ;;  nil
+   ;;  )
+   )
+ t
+ )
+
+(font-lock-add-keywords
+ 'js-mode
+ ;;'(("\\([a-zA-Z0-9_]+\\) *("
+ '(("\\([a-z][a-zA-Z0-9]*\\) *("
+    1
+    'font-lock-function-name-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([a-z][a-zA-Z0-9]*\\)"
+    1
+    'font-lock-variable-name-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([A-Z][a-zA-Z0-9_]*\\)"
+    1
+    'font-lock-type-face
+    nil
+    )
+   )
+ t
+ )
+
 ;;(highlight-regexp "[a-zA-Z1-9]")
 ;;(unhighlight-regexp "[a-zA-Z1-9]")
