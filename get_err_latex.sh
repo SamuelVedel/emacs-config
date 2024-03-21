8a02bccff878aca6c0cbf56fe5c8671c337f557c
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-03-21 23:08:54 +0100

    an error selection a litle bettor for latex-mode

--- get_err_latex.sh
+++ get_err_latex.sh
@@ -7,4 +7,4 @@ zut_file=~/emacs-config/zut.txt
 cd $folder
 pdflatex -halt-on-error $file 2> $zut_file > $zut_file && pdflatex -halt-on-error $file 2> /dev/null > /dev/null #&& $(evince $(echo $file | sed "s/\.tex/\.pdf/") &)
 
-cat $zut_file | grep "^l.[1-9]*" | cut -d '.' -f 2 | cut -d ' ' -f 1
+cat $zut_file | grep "^l.[1-9]* " | cut -d '.' -f 2 | cut -d ' ' -f 1
