a7fe26516ce93851f12414c19989cfd7f5f2601d
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-06-02 21:00:51 +0200

    [Feat](main)<get_err_java.sh> the scipt find a makefile and compile with the makefile

--- get_err_java.sh
+++ get_err_java.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
-# file=$(echo $1 | rev | cut -d '/' -f 1 | rev)
-# dir=$(echo $1 | rev | cut -d '/' -f 2- | rev)
+file=$(echo $1 | rev | cut -d '/' -f 1 | rev)
+dir=$(echo $1 | rev | cut -d '/' -f 2- | rev)
 
-# function go_back() {
-# 	file=$(echo $dir | rev | cut -d '/' -f 1 | rev)/$file
-# 	dir=$(echo $dir | rev | cut -d '/' -f 2- | rev)
-# }
+function go_back() {
+	file=$(echo $dir | rev | cut -d '/' -f 1 | rev)/$file
+	dir=$(echo $dir | rev | cut -d '/' -f 2- | rev)
+}
 
-file=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 2)
-dir=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 1)
+# file=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 2)
+# dir=$(echo $1 | sed 's/\/src\//;/g' | cut -d ';' -f 1)
 zut_file=~/emacs-config/zut.txt
 
+function find_makefile() {
+	while ! [ -f $dir/Makefile ] && [ -d $dir ]
+	do
+		go_back
+	done
+}
+
 function exec_make() {
-	#if [ -f $1/Makefile ]
-	#then
+	if [ -f $dir/Makefile ]
+	then
 		#echo $dir
 		#echo $file
-		#make -C $1 $(echo $file | sed 's/.java/.class/') 2> $zut_file > /dev/null
-		javac -encoding iso-8859-1 -d $dir/bin/ -cp $dir/src/ $dir/src/$file 2> $zut_file > /dev/null
-		cat $zut_file | grep /$file | cut -d ':' -f 2 | uniq
+		#echo $(echo $file | sed 's/\.java$/\.class/' | sed 's/src/bin/')
+		make -C $dir $(echo $file | sed 's/\.java$/\.class/' | sed 's/src/bin/') 2> $zut_file > /dev/null
+		#javac -encoding iso-8859-1 -d $dir/bin/ -cp $dir/src/ $dir/src/$file 2> $zut_file > /dev/null
+		cat $zut_file | grep $file | cut -d ':' -f 2 | uniq
 		exit
-	#fi
+	fi
 }
 
 #echo $file
 #echo $dir
-exec_make $dir
+find_makefile
+exec_make #$dir
