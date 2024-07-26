780773569d84a6137eec9e1534e964d9aef7f33b
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-07-26 03:41:31 +0200

    [Feat](main)<config.el> use major mode glsl-mode

--- config.el
+++ config.el
@@ -107,6 +107,16 @@
              '("\\.ts\\'"       . js-mode)
              )
 
+;; from https://raw.githubusercontent.com/jimhourihan/glsl-mode/master/glsl-mode.el
+;; or https://maverick.inria.fr/~Xavier.Decoret/resources/glsl-mode/glsl-mode.el
+(require 'glsl-mode)
+(add-to-list 'auto-mode-alist
+             '("\\.vert\\'"       . glsl-mode)
+             )
+(add-to-list 'auto-mode-alist
+             '("\\.frag\\'"       . glsl-mode)
+             )
+
 ;;-------------------------------------
 ;; Bordel
 
