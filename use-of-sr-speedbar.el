c8d4df353e71306dc36118e28958007a17ec752d
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-08-23 21:14:22 +0200

    [Update](main)<use-of-sr-speedbar.el> changed the comment about the link from the library

--- use-of-sr-speedbar.el
+++ use-of-sr-speedbar.el
@@ -1,4 +1,4 @@
-;; https://www.emacswiki.org/emacs/download/sr-speedbar.el
+;; from https://www.emacswiki.org/emacs/download/sr-speedbar.el
 (require 'sr-speedbar)
 (setq sr-speedbar-right-side nil) ;; on the left side
 (setq sr-speedbar-auto-refresh nil) ;; no auto refresh
