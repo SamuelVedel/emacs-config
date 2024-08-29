35752462f6e6135c7a2d10163962420b99e59705
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-08-29 22:02:57 +0200

    [feat](main)<use-of-glsl-mode.el> add syntax coloration to glsl-mode

--- use-of-glsl-mode.el
+++ use-of-glsl-mode.el
@@ -7,3 +7,29 @@
 (add-to-list 'auto-mode-alist
              '("\\.frag\\'"       . glsl-mode)
              )
+
+(font-lock-add-keywords
+ 'glsl-mode
+ '(("\\([a-z][a-zA-Z0-9]*\\) *("
+    1
+    'font-lock-function-call-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([a-z][a-zA-Z0-9]*\\)"
+    1
+    'font-lock-variable-use-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([A-Z][A-Z0-9_]*\\)[^a-zA-Z0-9_]"
+    1
+    'font-lock-constant-face
+    nil
+    )
+   ("[^a-zA-Z0-9_]\\([A-Z][a-zA-Z0-9]*\\)[^a-zA-Z_]"
+    1
+    'font-lock-type-face
+    nil
+    )
+   )
+ t
+ )
