6af289b1839e826d73d16716717eaaf878576e1f
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2025-02-27 13:48:32 +0100

    [Update](main)<use-of-{lsp-mode,consul}.el> change lsp mode keybindings and conflicts

--- use-of-consult.el
+++ use-of-consult.el
@@ -1,2 +1,2 @@
 (global-set-key (kbd "C-x b") 'consult-buffer)
-(global-set-key (kbd "C-c l") 'consult-goto-line)
+(global-set-key (kbd "C-c g l") 'consult-goto-line)
--- use-of-lsp-mode.el
+++ use-of-lsp-mode.el
@@ -1,7 +1,15 @@
 (require 'lsp-mode)
 
+;; To remove the lsp topbar
 (setq lsp-headerline-breadcrumb-enable nil)
+;; to kill the server after using it
+(setq lsp-keep-workspace-alive nil)
 
 ;; enable wich-hey integration
 (with-eval-after-load 'lsp-mode
   (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
+
+;; change the lsp keymap prefix from s-l to C-c l
+;; because s-l don't work when emacs is running in a terminal
+(setq lsp-keymap-prefix "C-c l")
+(define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
