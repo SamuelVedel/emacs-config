d0c11f3a77e24c9c8af7df4238b2b398103568a1
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2025-04-19 01:15:37 +0200

    [Update](main)<mlng-mode.el> add the sizeof keyword

--- mlng-mode.el
+++ mlng-mode.el
@@ -1,7 +1,7 @@
 ;; define mlng major mode
 
 (defconst mlng--font-lock-defaults
-    '((("\\<\\(const\\|if\\|while\\|do\\|break\\|continue\\|return\\)\\>" 0 font-lock-keyword-face)
+    '((("\\<\\(const\\|if\\|while\\|do\\|break\\|continue\\|return\\|sizeof\\)\\>" 0 font-lock-keyword-face)
        ("\\<\\([a-z][a-z0-9_]*\\)\s*(" 1 font-lock-function-name-face)
        ("\\.*\\(int1\\|int2\\|int4\\|int8\\|uint1\\|uint2\\|uint4\\|uint8\\|flt4\\|flt8\\|void\\)\\>" 0 font-lock-type-face)
        ("\\<[a-z][a-z0-9_]*\\>" 0 font-lock-variable-name-face)
