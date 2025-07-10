2883dcfbc8bbafc0fccc3179d5ca6b883f4bf67e
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2025-07-10 23:15:45 +0200

    [Feat](main)<config.el> add keybindings for incrementing values

--- config.el
+++ config.el
@@ -225,6 +225,81 @@
 (setq scroll-step 1)
 (setq scroll-margin 5)
 
+
+;;;;;;;;;;;;;;; To increment numbers
+;; found on https://www.emacswiki.org/emacs/IncrementNumber
+
+(defun increment-number-decimal (&optional increment)
+  "Increment the number at point by INCREMENT."
+  (interactive "*nIncrement: ")
+  (let ((pos (point)))
+    (save-match-data
+      (skip-chars-backward "0-9")
+      (if (looking-at "[0-9]+")
+          (let ((field-width (- (match-end 0) (match-beginning 0)))
+                (newval (+ (string-to-number (match-string 0) 10) increment)))
+            (when (< newval 0)
+              (setq newval (+ (expt 10 field-width) newval)))
+            (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                   newval)))
+        (user-error "No number at point")))
+    (goto-char pos)))
+
+(defun increment-number-hexadecimal (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "*nIncrement: ")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer hex-format)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789abcdefABCDEF")
+        (when (re-search-forward "[0-9a-fA-F]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 16) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 16 field-width) answer)))
+          (if (equal (match-string 0) (upcase (match-string 0)))
+              (setq hex-format "X")
+            (setq hex-format "x"))
+          (replace-match (format (concat "%0" (int-to-string field-width)
+                                         hex-format)
+                                 answer)))))))
+
+(defun format-bin (val width)
+  "Convert a number to a binary string."
+  (let (result)
+    (while (> width 0)
+      (if (equal (mod val 2) 1)
+          (setq result (concat "1" result))
+        (setq result (concat "0" result)))
+      (setq val (/ val 2))
+      (setq width (1- width)))
+    result))
+
+(defun increment-number-binary (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "*nIncrement: ")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "01")
+        (when (re-search-forward "[0-1]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 2) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 2 field-width) answer)))
+          (replace-match (format-bin answer field-width)))))))
+
+(global-set-key (kbd "C-c + d") (lambda () (interactive) (increment-number-decimal 1)))
+(global-set-key (kbd "C-c + + d") 'increment-number-decimal)
+
+(global-set-key (kbd "C-c + h") (lambda () (interactive) (increment-number-hexadecimal 1)))
+(global-set-key (kbd "C-c + + h") 'increment-number-hexadecimal)
+
+(global-set-key (kbd "C-c + b") (lambda () (interactive) (increment-number-binary 1)))
+(global-set-key (kbd "C-c + + b") 'increment-number-binary)
+
 ;;-------------------------------------
 ;; Add mods
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . js-mode))
