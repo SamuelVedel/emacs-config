bd8e31a39ff5634c7c1b5a3b24e73143ee47d00d
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2026-03-04 20:12:43 +0100

    [Update](main)<use-of-markdown-mode.el> prevent markdown-mode from overriding scrolling keybindings

--- use-of/use-of-markdown-mode.el
+++ use-of/use-of-markdown-mode.el
@@ -1,1 +1,8 @@
-(add-hook 'markdown-mode-hook #'visual-line-mode) ;; line that break smoothly
+(add-hook 'markdown-mode-hook #'visual-line-mode) ;; lines that break smoothly
+
+;; to prevent markdown from overriding scrolling keybindings
+(add-hook 'markdown-mode-hook 
+          (lambda ()
+            (define-key markdown-mode-map (kbd "M-p") nil)
+            (define-key markdown-mode-map (kbd "M-n") nil)
+            ))
