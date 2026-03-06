d9265e0cb27a0013b0296917cd361283a2d9be66
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2026-03-06 19:28:47 +0100

    [Feat](main)<use-of-which-key.el> add name to custom prefix for which key

--- use-of/use-of-which-key.el
+++ use-of/use-of-which-key.el
@@ -3,3 +3,13 @@
 (which-key-mode)
 ;;(which-key-setup-minibuffer)
 (which-key-setup-side-window-bottom)
+
+(which-key-add-key-based-replacements
+  "C-c l" "long lines"
+  "C-c n" "line numbering"
+  "C-c +" "increment"
+  "C-c + +" "increment of n"
+  "C-c c" "complete"
+  "C-c g" "goto"
+  "C-c m" "make"
+  "C-c p" "print completion")
